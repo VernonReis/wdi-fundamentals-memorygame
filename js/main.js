9ab194c0525b02330de1a5765345b0824fdf1283
venom9122 <32598819+venom9122@users.noreply.github.com>
2017-10-16 10:05:47 -0700

    Created game logic
    redistributed code into functions for later DOM implementation.

--- js/main.js
+++ js/main.js
@@ -5,24 +5,28 @@ console.log("Up and running!");
 var cards = ["queen","queen","king","king"];
 var cardsInPlay = [];
 
-var cardOne = cards[0];
-var cardTwo = cards[1];
-
-cardsInPlay.push(cardOne);
-console.log("User flipped " + cardOne);
-
-cardsInPlay.push(cardTwo);
-console.log("User flipped " + cardTwo);
-
-if(cardsInPlay.length === 2) 
-{
-	if(cardsInPlay[0] === cardsInPlay[1])
-	{
-		alert("You found a match!");
-	}
-	else
-	{
-		alert("Sorry, try again.");
+var checkForMatch = function() {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+		console.log("You found a match!");
+	} 
+	else {
+		console.log("Sorry, try again.");
 	}
 }
 
+
+var flipCard = function(cardID) {
+
+	
+	console.log("User flipped " + cards[cardID]);
+	cardsInPlay.push(cards[cardID]);
+
+	if(cardsInPlay.lenght === 2)
+	{
+		checkForMatch();
+	}
+}
+
+
+flipCard(0);
+flipCard(2);
